cc8359ac8539e13339bbdddf01c12c56f6184570
Leo Barnuevo <leocoding.net@gmail.com>
2024-11-14 08:25:55 -0600

    first commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello Wolrd");
+
+//1. Get the string from the page
+//controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+
+//2. Reverse the string
+//logic functiuon
+function reverseString(userString){
+    let revString = [];
+
+    for (let index = userString.length -1; index >= 0; index--) {
+       
+        revString += userString[index];       
+    }
+
+    return revString;
+}
+
+//3. Display the reversed message
+//view function
+function displayString(revString){
+    document.getElementById("msg").innerHTML = `Your string spelled backwards is <strong> ${revString}</strong>`;
+    document.getElementById("alert").classList.remove("invisible");
+
 }
